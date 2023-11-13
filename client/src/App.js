045619e2e4edfc4ff8f3d604fc3947181e76f296
Praveen Kumar Dande <dpraveenkumar511@gmail.com>
2023-11-13 16:03:19 -0500

    changed the base path to host on hpg

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import logo from './Images/SUNLab_Logo.webp';
 import './App.css';
 import Header from './Components/Header.js'
 import ListFiles from './Components/ListFiles.js';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
+    <Router basename="/api">
     <div className="App">
       <Header/>
       <ListFiles></ListFiles>
     </div>
+    </Router>
   );
 }
 
