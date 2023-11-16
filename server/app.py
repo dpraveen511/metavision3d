461f6c3832e6f5b132e6785b59cd0dea5cab6565
Praveen Kumar Dande <dpraveenkumar511@gmail.com>
2023-11-16 16:46:59 -0500

    changed the flask port

--- server/app.py
+++ server/app.py
@@ -109,4 +109,4 @@ scheduler.add_job(func=check_files, trigger="interval", minutes=1)
 scheduler.start()
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=19291)
