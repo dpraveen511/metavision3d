7c461f2fe298aecd83ad9f9ca8059d3be23e6c20
Praveen Kumar Dande <dpraveenkumar511@gmail.com>
2023-11-30 04:04:44 -0500

    embedded the videos

--- client/src/Components/ListFiles.js
+++ client/src/Components/ListFiles.js
@@ -218,7 +218,7 @@ function ListFiles() {
             max={maxIntensity}
             session_id = {sessionId}
             ></DisplayFile>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
+            {/* <div style={{ display: 'flex', alignItems: 'center' }}>
             <div style={{ textAlign: 'left', paddingLeft: '20px' }}>
                 <Button 
                     color="warning" 
@@ -229,7 +229,29 @@ function ListFiles() {
                 </Button>
             </div>
             
+            </div> */}
+            <hr></hr>
+            <div class="container my-4">
+            <div class="row">
+        <div class="col-12">
+            <h2 class="text-center mb-4">Tutorials</h2>
+        </div>
+    </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="video-responsive">
+                    
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/u5dQYSZw-fY?si=I5_WcxYgT0c4fcI0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
             </div>
+            <div class="col-md-6">
+                <div class="video-responsive">
+                    
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/mvj_ehIT0MY?si=DINUEPM618sgd2ZN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
         </div>
     );
 }
